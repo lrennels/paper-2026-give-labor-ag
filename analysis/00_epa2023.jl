fd7cad321d6214276337e1051bfc5a17bcc5a56b
lisarennels <lrennels@berkeley.edu>
2026-04-21 21:59:30 -0700

    Use 2025 pulse year for SCC computation

---
+++ analysis/00_epa2023.jl
@@ -0,0 +1,68 @@
+using Mimi, MimiGIVE, Random, CSVFiles, DataFrames, Statistics
+
+# set emissions year
+year = 2025;
+
+# choose damage module
+damages = :give;
+
+# choose gas
+gas = :CO2;
+
+# set named list of discount rates
+discount_rates = 
+    [
+        (label = "1.5% Ramsey", prtp = exp(0.000091496)-1, eta  = 1.016010261),
+        (label = "2.0% Ramsey", prtp = exp(0.001972641)-1, eta  = 1.244459020),
+        (label = "2.5% Ramsey", prtp = exp(0.004618785)-1, eta  = 1.421158057)
+    ];
+
+# read the series of rffsp-fair pairings. these were randomly selected pairings. read GIVE documentation for other functionality.
+rffsp_fair_sequence = load(joinpath(@__DIR__, "../data/epa2023/rffsp_fair_sequence.csv")) |> DataFrame;
+
+fair_parameter_set_ids = (rffsp_fair_sequence.fair_id)[1:num_trials];
+rffsp_sampling_ids     = (rffsp_fair_sequence.rffsp_id)[1:num_trials];
+
+# set random seed
+Random.seed!(seed);
+
+# get model 
+m = MimiGIVE.get_model();
+
+# estimate scghg
+results = 
+    MimiGIVE.compute_scc(m, 
+                        n                       = num_trials, 
+                        gas                     = gas, 
+                        year                    = year, 
+                        pulse_size              = 0.0001,                   # scales the default pulse size of 1Gt to 100k metric tons 
+                        certainty_equivalent    = false,                     
+                        fair_parameter_set      = :deterministic,           # optionally read the rffsp-fair parameter sequence from file
+                        fair_parameter_set_ids  = fair_parameter_set_ids,   # optionally read the rffsp-fair parameter sequence from file
+                        rffsp_sampling          = :deterministic,           # optionally read the rffsp-fair parameter sequence from file
+                        rffsp_sampling_ids      = rffsp_sampling_ids,       # optionally read the rffsp-fair parameter sequence from file
+                        CIAM_GDPcap             = true, 
+                        discount_rates          = discount_rates, 
+                        save_slr_damages        = false,                    # save coastal damages, comparable to including DamageAggregator components in save_list
+                        save_cpc                = false,                    # must be true to recover certainty equivalent scghgs
+                        compute_domestic_values = false,
+                        compute_sectoral_values = true);
+
+# blank data
+scghgs = DataFrame(sector = String[], discount_rate = String[], trial = Int[], scghg = Float64[]);
+    
+# populate data
+for (k, v) in results[:scc]
+    for (i, sc) in enumerate(v.sccs)
+        push!(scghgs, (sector = String(k.sector), discount_rate = k.dr_label, trial = i, scghg = sc*pricelevel_2005_to_2020))
+    end
+end
+
+# export full distribution    
+scghgs |> save(joinpath(@__DIR__, "../output/epa2023/sc-$gas-$damages-$year-n$num_trials.csv"));
+
+# collapse to the certainty equivalent scghgs
+scghgs_mean = combine(groupby(scghgs, [:sector, :discount_rate]), :scghg => (x -> round(Int, mean(x))) .=> :scghg)
+
+# export average scghgs    
+scghgs_mean |> save(joinpath(@__DIR__, "../output/epa2023/sc-$gas-$damages-$year.csv"));
--- analysis/main.jl
+++ analysis/main.jl
@@ -3,6 +3,7 @@ Pkg.activate(joinpath(@__DIR__, ".."))
 
 using Mimi, VegaLite, Random, Query, DataFrames, Dates
 
+const pricelevel_2005_to_2025 = 128.979 / 81.556 # 4/21/2026 BEA Table 1.1.9, line 1 GDP annual values as linked here: https://apps.bea.gov/iTable/iTable.cfm?reqid=19&step=3&isuri=1&select_all_years=0&nipa_table_list=13&series=a&first_year=2005&last_year=2025&scale=-99&categories=survey&thetable=
 const pricelevel_2005_to_2020 = 113.648 / 87.504 # from Rennert et al. 2022 and (10/25/2021) BEA Table 1.1.9, line 1 GDP annual values as linked here: https://apps.bea.gov/iTable/iTable.cfm?reqid=19&step=3&isuri=1&select_all_years=0&nipa_table_list=13&series=a&first_year=2005&last_year=2020&scale=-99&categories=survey&thetable=
 const seed = 24523438 # set any seed for reproducibility
 
@@ -12,15 +13,19 @@ include(joinpath(@__DIR__, "..", "src", "main.jl"))
 # Settings
 year = 2020 # emissions year
 num_trials = 10_000
-discount_rates = [ # from Rennert et al. 2022
-    (label = "1.5%", prtp = exp(9.149606e-05) - 1, eta = 1.016010e+00),
-    (label = "2.0%", prtp = exp(0.001972641) - 1, eta = 1.244458999),
-    (label = "2.5%", prtp = exp(0.004618784) - 1, eta = 1.421158088),
-    (label = "3.0%", prtp = exp(0.007702711) - 1, eta = 1.567899391)
-]
+discount_rates = [
+                    (label = "1.5%", prtp = exp(9.149606e-05) - 1, eta = 1.016010e+00),
+                    (label = "2.0%", prtp = exp(0.001972641) - 1, eta = 1.244458999),
+                    (label = "2.5%", prtp = exp(0.004618784) - 1, eta = 1.421158088),
+                    (label = "3.0%", prtp = exp(0.007702711) - 1, eta = 1.567899391)
+                ] # Rennert et al. 2022
 labor_functions = ["Lancet", "ISO"]
 
 # Run analysis
+
+@info "Starting epa 2023 analysis at $(now())"
+include("00_epa2023.jl")
+
 @info "Starting SCC analysis at $(now())"
 include("01_compute_scc.jl")
 
